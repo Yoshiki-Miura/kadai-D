1c350377d35200448d881df3c4a001ce3c2a1f24
yoshiki-san <yy.bb.tf@gmail.com>
2019-09-25 16:07:15 +0900

    strメソッドは結構簡単に見つけることができた。

--- D-2.py
+++ D-2.py
@@ -18,35 +18,12 @@ class SemanticVersion:
     def __str__(self):
         return f"{self.major}.{self.minor}.{self.patch}"
 
-    def patch_version_up(self):
-        self.patch += 1
-        return SemanticVersion(self.major, self.minor, self.patch)
-
-    def _version_up(self):
-        self.patch += 1
-        return SemanticVersion(self.major, self.minor, self.patch)
-
 
 def main():
     # Python3.7.0 というバージョンを表現したもの
     py370 = SemanticVersion(major=3, minor=7, patch=0)
 
-    # 等価である場合
-    print(SemanticVersion(3, 7, 0) == py370)  # True
-
-    # 等価でない場合
-    print(SemanticVersion(3, 7, 1) == py370)  # False
-
-    # Python3.7.0 というバージョンを表現したもの
-    py370 = SemanticVersion(major=3, minor=7, patch=0)
-
     print('3.7.0' == str(py370))  # True
-
-    # Python3.7.0 というバージョンを表現したもの
-    py370 = SemanticVersion(major=3, minor=7, patch=0)
-
-    py371 = py370.patch_version_up()
-    print(SemanticVersion(3, 7, 1) == py371)  # True
 
 
 if __name__ == '__main__':

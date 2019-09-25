5275f546063c1a6fb9b259da98fd1b3c6b4a5790
yoshiki-san <yy.bb.tf@gmail.com>
2019-09-25 16:11:17 +0900

    課題1に比べればかなり簡単だった。

---
+++ D-4.py
@@ -0,0 +1,40 @@
+class SemanticVersion:
+    def __init__(self, major, minor, patch):
+        self.major = major
+        self.minor = minor
+        self.patch = patch
+
+    def __eq__(self, other):
+        if not isinstance(other, SemanticVersion):
+            return NotImplemented
+        if self.major != other.major:
+            return False
+        if self.minor != other.minor:
+            return False
+        if self.patch != other.patch:
+            return False
+        return True
+
+    def __str__(self):
+        return f"{self.major}.{self.minor}.{self.patch}"
+
+    def patch_version_up(self):
+        self.patch += 1
+        return SemanticVersion(self.major, self.minor, self.patch)
+
+    def minor_version_up(self):
+        self.minor += 1
+        self.patch = 0
+        return SemanticVersion(self.major, self.minor, self.patch)
+
+
+def main():
+    # Python3.7.0 というバージョンを表現したもの
+    py370 = SemanticVersion(major=3, minor=7, patch=0)
+
+    py380 = py370.minor_version_up()
+    print(SemanticVersion(3, 8, 0) == py380)  # True
+
+
+if __name__ == '__main__':
+    main()
